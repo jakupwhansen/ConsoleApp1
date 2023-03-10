b9497d934560e5ea0478934a2d97d4aa7bb82a99
Jákup Wenningstedt Hansen <jakupwhansen@hotmail.com>
2023-03-10 11:19:58 +0100

    simplificeret filen

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,26 +3,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("World!");
-        Console.WriteLine("God");
-        Console.WriteLine("farvel");
-        Console.WriteLine("der var");
-        Console.WriteLine("linje 5");
-        Console.WriteLine("linje 6");
-        Console.WriteLine("linje 7");
-        Console.WriteLine( "linje 8");
-        Console.WriteLine("linje 9");
-        Console.WriteLine("linje 10 fra Jakup2");
-        Console.WriteLine("linje 11 fra Jakup2");
-        Console.WriteLine("linje 12 fra Jakup2");
-        Console.WriteLine("09:45 fra Jakup2");
-        Console.WriteLine("09:52 fra Jakup2");
-        Console.WriteLine("09:55 fra Jakup2");
-        Console.WriteLine("10:02 fra Jakup2");
-        Console.WriteLine("10:25 fra Jakup2");
-        Console.WriteLine("10:33 fra Jakup2");
-        Console.WriteLine("10:50 fra Jakup2");
-        Console.WriteLine("10:53 fra Jakup2");
         Console.WriteLine("11:03 fra Jakup2");
     }
 }
